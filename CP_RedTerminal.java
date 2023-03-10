fd9a704535d34e16fb94d5808702ecf40809a589
GaugeCaywood <94421618+GaugeCaywood@users.noreply.github.com>
2023-03-10 11:03:37 -0700

    Update CP_RedTerminal.java

--- CP_RedTerminal.java
+++ CP_RedTerminal.java
@@ -17,18 +17,19 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 //import org.firstinspires.ftc.teamcode.Drive;
 //import org.firstinspires.ftc.teamcode.Duck;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import java.lang.Math;
 import java.util.ArrayList;
 
 
-@Autonomous(name="2CP_RedTerminal", group="Auton")
-public class CP_RedTerminal extends LinearOpMode {
+@Autonomous(name="CP_RED_Terminal_DS", group="Auton")
+public class CP_RedTerminalDS extends LinearOpMode {
 
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
     BotHardware robot = new BotHardware();
-
+    public ElapsedTime runtime = new ElapsedTime();
     static final double FEET_PER_METER = 3.28084;
 
     double fx = 578.272;
@@ -38,7 +39,7 @@ public class CP_RedTerminal extends LinearOpMode {
 
     // UNITS ARE METERS
     double tagsize = 0.166;
-    private ElapsedTime runtime = new ElapsedTime();
+//    private ElapsedTime runtime = new ElapsedTime();
 
     //    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
 //Tag ID's of sleeve.
@@ -56,6 +57,7 @@ public class CP_RedTerminal extends LinearOpMode {
         TRAJ_1,
         TRAJ_BACK,
         TURN_1,
+        SENSOR_CHECK,
         TRAJ_2,
         TRAJ_3,
         TURN_2,
@@ -63,23 +65,37 @@ public class CP_RedTerminal extends LinearOpMode {
         TRAJ_5,
         TURN_3,
         TRAJ_6,
-        PARK
+        TRAJ_7,
+        TURN_4,
+        PARK,
+        IDLE
     };
 
     MotorState motorState = MotorState.LVL;
-    State currentState = State.TRAJ_1;
+    State currentState = State.IDLE;
 
     //TOWER ARRAY
-    int[] towerPos = {20, -90, -247, -342, -470};
+    int[] towerPos = {20, -90, -247, -342, -450};
 
     ElapsedTime liftTimer = new ElapsedTime();
 
+    //Sensor variables
+    double sensorUp = 1;            double sensorDown = 0.5;
+    double minDSPoleTurn = 25;    double maxDSPoleTurn = 15;
+    double minDSPoleFWD = 3;        double maxDSPoleFWD = 4;
+    double minDsPoleBWD = 20;        double maxDSPoleBWD = 7;
+    double minDSTowerTurn = 100;   double maxDSTowerTurn = 15;
+    double minDSTowerFWD = 3;    double maxDSTowerFWD = 10;
+    double minDsTowerBWD = 300;        double maxDSTowerBWD = 400;
+    //Lift variables
     int c = 4;
     int target = 0;
     boolean GoPos = true;
+
     // LIFT POSITIONS
     public void Update(int target){
 //        drive.lift.switchToLevel(target);
+        telemetry.update();
         robot.lift.setPower(-1);
         StateUpdate(true, target);
     }
@@ -127,7 +143,7 @@ public class CP_RedTerminal extends LinearOpMode {
         ///// TRAJECTORIES /////
         //FORWARD
         Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
-                .forward(66)
+                .forward(60)
                 .build();
 
         Trajectory trajBack = drive.trajectoryBuilder(new Pose2d())
@@ -136,45 +152,37 @@ public class CP_RedTerminal extends LinearOpMode {
 
         //FORWARD AFTER TURN
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .forward(9)
+                .forward(5)
                 .build();
 
         //BACK THEN TURN
         Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
-                .back(13)
+                .back(14)
                 .addTemporalMarker(0, () -> {
                     resetRuntime();
                     drive.R1.setPower(-1.0);
                     drive.L1.setPower(-1.0);
                 })
                 .build();
-        //FORWARD TO CONES
-        Trajectory traj4 = drive.trajectoryBuilder(traj3.end())
-                .forward(30)
-                .addTemporalMarker(1, () -> {
+
+        //BACK THEN TURN
+        Trajectory traj7 = drive.trajectoryBuilder(traj2.end())
+                .back(11.5)
+                .addTemporalMarker(0, () -> {
                     resetRuntime();
-                    drive.R1.setPower(1.0);
-                    drive.L1.setPower(1.0);
+                    drive.R1.setPower(-1.0);
+                    drive.L1.setPower(-1.0);
                 })
                 .build();
-        // BACK OFF THE CONES
-        Trajectory traj5 = drive.trajectoryBuilder(traj4.end())
-                .back(30)
-                .addTemporalMarker(1, () -> {
-                    resetRuntime();
-                    drive.R1.setPower(0);
-                    drive.L1.setPower(0);
-                })
+
+        Trajectory p1 = drive.trajectoryBuilder(traj7.end())
+                .back(27)
                 .build();
-        //FORWARD AND COLLECT
-        Trajectory traj6 = drive.trajectoryBuilder(traj5.end())
-                .forward(9)
+        Trajectory p2 = drive.trajectoryBuilder(traj7.end())
+                .back(5)
                 .build();
-        Trajectory p1 = drive.trajectoryBuilder(traj6.end())
-                .forward(10)
-                .build();
-        Trajectory p3 = drive.trajectoryBuilder(traj6.end())
-                .back(9)
+        Trajectory p3 = drive.trajectoryBuilder(traj7.end())
+                .forward(23)
                 .build();
 
         //////APRIL TAGS DETECTION/////
@@ -290,146 +298,231 @@ public class CP_RedTerminal extends LinearOpMode {
 
         //start moving
         currentState = State.TRAJ_1;
+        robot.distancePoleServo.setPosition(sensorUp);
         drive.followTrajectoryAsync(traj1);
-        //target = -2900;
 
         while (opModeIsActive() && !isStopRequested()) {
             Update(target);
             switch(currentState){
                 case TRAJ_1:
-                    //Once turn is finished (not busy), move to turn 1
-                    //target = -2900;
-                    //Update(target);
-
                     if (!drive.isBusy()) {
-                        currentState = State.TRAJ_BACK;
                         drive.followTrajectoryAsync(trajBack);
+                        currentState = State.TRAJ_BACK;
                     }
                     break;
                 case TRAJ_BACK:
-                    target = -2900;
+                    target = -2950;
+                    telemetry.addData("Currently running: ", currentState);
                     Update(target);
                     if (!drive.isBusy()) {
-                        currentState = State.TURN_1;
-                        drive.turnAsync(Math.toRadians(-65));
+                        if(robot.lift.getCurrentPosition() < -600) {
+                            robot.distancePoleServo.setPosition(sensorDown);
+                        }
+                        drive.turnAsync(Math.toRadians(-25)); //Turn a bit before searching for pole
+                        currentState = State.SENSOR_CHECK;
                     }
+                case SENSOR_CHECK:
+                    target = -2950;
+                    Update(target);
+                    telemetry.addData("Currently running: ", currentState);
+                    Update(target);
+                    if (!drive.isBusy()) {
+                        rightTurn(.2, minDSPoleTurn, maxDSPoleTurn);
+                        currentState = State.TURN_1;
+                    }
+
                 case TURN_1:
-                    target = -2900;
+                    target = -2950;
                     Update(target);
 
                     //Once turn is finished, move to traj 2
                     if (!drive.isBusy()) {
-                        currentState = State.TRAJ_2;
+                        robot.distancePoleServo.setPosition(sensorUp);
                         drive.followTrajectoryAsync(traj2);
+                        currentState = State.TRAJ_2;
                     }
                     break;
                 case TRAJ_2:
                     //Once traj 2 is finished (and over pole), drop lift to tower 5, move to traj 3
-                    target = -2900;
-                    Update(target);
-
                     if(!drive.isBusy()){
-                        target = towerPos[c];
+                        target = -2050;
+                        Update(target);
+                        drive.followTrajectoryAsync(traj7);
                         currentState = State.TRAJ_3;
-                        drive.followTrajectoryAsync(traj3);
+
                     }
                     break;
                 case TRAJ_3:
-                    //Once traj 3 is finished, move to turn 2
-                    target = towerPos[c];
-
                     if (!drive.isBusy()) {
-                        if(c == 3){
-                            currentState = State.PARK;
-                        }else{
-                            currentState = State.TURN_2;
-                        }
-                        drive.turnAsync(Math.toRadians(140));
+                        resetRuntime();
+                        drive.turnAsync(Math.toRadians(95));
+                        currentState = State.TURN_2;
                     }
                     break;
                 case TURN_2:
-                    //Once turn 2 is finished, move to traj 4 (forward into tower)
-                    target = towerPos[c];
                     Update(target);
 
-                    if(!drive.isBusy()){
-                        c = c-1; //also drop tower counter so it'll be correct next loop
-                        currentState = State.TRAJ_4;
-                        drive.followTrajectoryAsync(traj4);
-                    }
-                    break;
-                case TRAJ_4:
-                    //Once traj 4 is finished, lift to high level and move to traj 5 (this should lift early enough to not tip the tower but we may need a slight wait)
-                    if(!drive.isBusy()){
-                        target = -2900;
-                        currentState = State.TRAJ_5;
-                        drive.followTrajectoryAsync(traj5);
-                    }
-                    break;
-                case TRAJ_5:
-                    //Once traj 5 is finished, move to turn 3
-                    target = -2900;
-
-                    if (!drive.isBusy()) {
-                        currentState = State.TURN_3;
-                        drive.turnAsync(Math.toRadians(-140));
-                    }
-                    break;
-                case TURN_3:
-                    //Once turn 3 is finished, move to traj 6
-                    target = -2900;
-
-                    if (!drive.isBusy()) {
-                        currentState = State.TRAJ_6;
-                        drive.followTrajectoryAsync(traj6);
-                    }
-                    break;
-                case TRAJ_6:
-                    //Once traj 6 is finished, check if we still have cones on the tower (c >= 0).
-                    //If so, drop to next tower pos and repeat up to Traj2 (which goes into traj3/drop and back from pole)
-                    if(!drive.isBusy() && (c >= 3)){
-                        target = towerPos[c];
-                        currentState = State.TRAJ_2;
-                    }else if(!drive.isBusy()){ //If not, move to idle state.
+                    if(!drive.isBusy()) {
+                        leftTurnTower(.25, minDSTowerTurn, maxDSTowerTurn);
                         currentState = State.PARK;
                     }
-                    break;
                 case PARK:
                     if(!drive.isBusy()){
+                        robot.distancePoleServo.setPosition(sensorUp);
                         drive.wrist.setPosition(0);
+                        drive.L1.setPower(0);
+                        drive.R1.setPower(0);
+                        target = -11;
+                        Update(target);
                     }
                     if(tagOfInterest.id == pos1){
                         //park 1
                         if (!drive.isBusy()) {
-                            drive.followTrajectoryAsync(p1);
+                            target = -11;
+                            Update(target);
+                            drive.turnAsync(Math.toRadians(-20));
+                            drive.followTrajectoryAsync(p3);
+                            currentState = State.IDLE;
                         }
                     }else if(tagOfInterest.id == pos2){
                         //park 2
+                        if (!drive.isBusy()) {
+                            target = -11;
+                            Update(target);
+                            drive.followTrajectoryAsync(p2);
+                            currentState = State.IDLE;
+                        }
                     }else{
                         //park 3
                         if (!drive.isBusy()) {
-                            drive.followTrajectoryAsync(p3);
+                            target = -11;
+                            Update(target);
+                            drive.turnAsync(Math.toRadians(-20));
+                            drive.followTrajectoryAsync(p1);
+                            currentState = State.IDLE;
                         }
+                    }
+                    break;
+                case IDLE:
+                    if (!drive.isBusy()) {
+                        // end, do nothing
                     }
                     break;
             }
 
             Update(target);
             drive.update();
-
-            if(tagOfInterest.id == pos1){
-                //park 1
-                drive.followTrajectoryAsync(p1);
-            }else if(tagOfInterest.id == pos2){
-                //park 2
-
-            }else{
-                //park 3
-
-                drive.followTrajectoryAsync(p3);
-            }
         }
 
+    }
+
+    public void leftTurn (double power, double minDS, double maxDS){
+        robot.turnLeft(power);
+        while(robot.distanceTower.getDistance(DistanceUnit.CM) > minDS && opModeIsActive()){
+            telemetry.addData("Currently turning LEFT in: ", currentState);
+            telemetry.addData("CM seen: ", robot.distanceTower.getDistance(DistanceUnit.CM));
+            Update(target);
+            if(robot.lift.getCurrentPosition() < -600) {
+                robot.distancePoleServo.setPosition(sensorDown);
+            }
+        }
+        robot.stop();
+        Update(target);
+    }
+
+    public void leftTurnManual (double time, double power){
+        Update(target);
+        robot.turnLeft(power);
+        resetRuntime();
+        while (opModeIsActive() && (runtime.seconds() < time)){
+            Update(target);
+            if(robot.lift.getCurrentPosition() < -600) {
+                robot.distancePoleServo.setPosition(sensorDown);
+            }
+        }
+        robot.stop();
+    }
+
+    public void leftTurnTower (double power, double minDS, double maxDS) {
+        if(robot.lift.getCurrentPosition() < -600) {
+            robot.distancePoleServo.setPosition(sensorDown);
+        }
+        robot.turnLeft(power);
+        while (robot.distanceTower.getDistance(DistanceUnit.CM) > minDS && opModeIsActive()) {
+            telemetry.addData("Currently turning RIGHT in: ", currentState);
+            telemetry.addData("CM seen: ", robot.distanceTower.getDistance(DistanceUnit.CM));
+            Update(target);
+        }
+        robot.stop();
+        Update(target);
+    }
+
+    public void rightTurn (double power, double minDS, double maxDS) {
+        resetRuntime();
+        while(runtime.milliseconds() < 1000) {
+            Update(target);
+        }
+
+        robot.turnRight(power);
+        while (robot.distanceTower.getDistance(DistanceUnit.CM) > minDS && opModeIsActive()) {
+            telemetry.addData("Currently turning RIGHT in: ", currentState);
+            telemetry.addData("CM seen: ", robot.distanceTower.getDistance(DistanceUnit.CM));
+            Update(target);
+        }
+        robot.stop();
+        Update(target);
+    }
+
+
+    public void forward (double power, double servoPower, double minDS, double maxDS){
+        while(robot.distanceTower.getDistance(DistanceUnit.CM) > minDS && opModeIsActive()){
+            //robot.distancePoleServo.setPosition(sensorUp);
+            Update(target);
+            if(robot.lift.getCurrentPosition() < -600) {
+                robot.distancePoleServo.setPosition(sensorDown);
+                robot.forward(power);
+                robot.L1.setPower(servoPower);
+                robot.R1.setPower(servoPower);
+            }
+        }
+        robot.stop();
+        robot.L1.setPower(0);
+        robot.R1.setPower(0);
+        Update(target);
+    }
+    public void back (double power, double minDS, double maxDS, double servoPower){
+        robot.backward(power);
+        //robot.distancePoleServo.setPosition(sensorUp);
+        while(robot.distanceTower.getDistance(DistanceUnit.CM) < minDS && opModeIsActive()){
+            Update(target);
+            if(robot.lift.getCurrentPosition() < -2550 && robot.lift.getCurrentPosition() > -400){
+                robot.distancePoleServo.setPosition(sensorUp);
+                robot.L1.setPower(servoPower);
+                robot.R1.setPower(servoPower);
+            }
+            if(robot.lift.getCurrentPosition() < -100 && robot.lift.getCurrentPosition() > -400){
+                robot.distancePoleServo.setPosition(sensorDown);
+                robot.L1.setPower(servoPower);
+                robot.R1.setPower(servoPower);
+            }
+        }
+        robot.stop();
+        Update(target);
+        robot.L1.setPower(0);
+        robot.R1.setPower(0);
+    }
+    public void slowForward(double power, double servoPower, double time){
+        resetRuntime();
+        Update(target);
+        robot.forward(power);
+        robot.L1.setPower(servoPower);
+        robot.R1.setPower(servoPower);
+        while(opModeIsActive() && runtime.seconds() < time){
+            Update(target);
+        }
+        robot.stop();
+        robot.L1.setPower(0);
+        robot.R1.setPower(0);
     }
 
     void tagToTelemetry(AprilTagDetection detection)
